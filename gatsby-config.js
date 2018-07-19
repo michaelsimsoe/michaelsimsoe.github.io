f7ef3f2d8a1735456dcedb86809cac702557e23a
Michael Krøysert-Simsø <mikke.korgen@gmail.com>
2018-07-19 22:08:44 +0200

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,25 @@ module.exports = {
   siteMetadata: {
     title: 'I like you a lot!',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.ico',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+  ],
 }
